2f2d0bb2ff00f6870d751cd3ad670c0f3d186a56
u2usvc <kwstisulvia@hotmail.com>
2025-07-22 14:41:13 +0300

    avoid domain SID collision by syspreping DC03

--- ansible/scripts/fix-winrm.ps1
+++ ansible/scripts/fix-winrm.ps1
@@ -2,7 +2,7 @@
 # check with echo '' | openssl s_client -connect 192.168.125.12:5986
 #
 Set-Service -Name "WinRM" -StartupType Automatic -Status Running
-$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "192.168.125.12"
+$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "WinRMCertificate"
 
 Get-ChildItem -Path WSMan:\LocalHost\Listener | Where-Object { $_.Keys -match 'Transport=HTTPS' } | Remove-Item -Recurse -Force
 New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
